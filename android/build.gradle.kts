fdae6dca60b40b3311f4991b6971566a70ddabd8
aprilianza <138241892+aprilianza@users.noreply.github.com>
2025-12-30 13:32:13 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,7 +11,16 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+
+    // PAKSA VERSI DI SINI agar berlaku untuk semua plugin (geolocator, image_picker, dll)
+    project.configurations.all {
+        resolutionStrategy {
+            force("androidx.activity:activity:1.9.3")
+            force("androidx.activity:activity-ktx:1.9.3")
+        }
+    }
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
